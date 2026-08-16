b94541cfbd8e3679e583db6e6f7fe7ea06040be1
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-08-16 15:02:11 +0900

    WindowSubmodule.cs 추가

---
+++ Runtime/WindowSubmodule.cs
@@ -0,0 +1,19 @@
+namespace Treehouse.Runtime;
+
+public class WindowSubmodule : EngineSubmodule
+{
+    internal override void OnInitialize()
+    {
+        base.OnInitialize();
+    }
+
+    internal override void OnTick()
+    {
+        base.OnTick();
+    }
+
+    internal override void OnRelease()
+    {
+        base.OnRelease();
+    }
+}
