bacf81d8fbb06022694ecb746399d12ae7767470
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-08-16 14:39:10 +0900

    EngineSubmodule.cs 추가

---
+++ Runtime/EngineSubmodule.cs
@@ -0,0 +1,19 @@
+namespace Treehouse.Runtime;
+
+public class EngineSubmodule
+{
+    internal virtual void OnInitialize()
+    {
+
+    }
+
+    internal virtual void OnTick()
+    {
+
+    }
+
+    internal virtual void OnRelease()
+    {
+
+    }
+}
