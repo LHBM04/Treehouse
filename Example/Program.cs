da4fa254b1194ed9143602307b619e8224c18b8a
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-08-16 16:56:53 +0900

    창 생성 로직 테스트

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,9 +1,27 @@
+using Treehouse.Runtime.System;
+using Treehouse.Runtime.System.Windowing;
+
 namespace Treehouse.Example;
 
 public class Program
 {
     private static void Main(string[] args)
     {
+        bool isRunning = true;
 
+        using (Engine engine = new Engine())
+        {
+            engine.AddSubmodule<WindowSubmodule>();
+
+            WindowSubmodule windowSubmodule = engine.GetSubmodule<WindowSubmodule>()!;
+            windowSubmodule.OnWindowRemoved += _ => isRunning = false;
+
+            windowSubmodule.CreateWindow(new WindowOptions());
+
+            while (isRunning)
+            {
+                engine.Tick();
+            }
+        }
     }
 }
