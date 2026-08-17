2bb03934ba33966cb510d3c646647f1d42db1d3e
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-08-17 13:01:10 +0900

    RenderSubsystem.cs 추가

---
+++ Runtime/System/Rendering/RenderSubsystem.cs
@@ -0,0 +1,14 @@
+namespace Treehouse.Runtime.System.Rendering;
+
+public class RenderSubsystem : EngineSubsystem
+{
+    internal override void OnInitialize()
+    {
+        base.OnInitialize();
+    }
+
+    internal override void OnRelease()
+    {
+        base.OnRelease();
+    }
+}
