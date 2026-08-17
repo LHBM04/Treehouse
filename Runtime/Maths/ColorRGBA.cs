c742cb267764cce0e22530896a7499130f1a534b
Lee Hyeoun Beom <86977637+LHBM04@users.noreply.github.com>
2026-08-17 14:27:55 +0900

    ColorRGBA 내 생성자 추가

--- Runtime/Maths/ColorRGBA.cs
+++ Runtime/Maths/ColorRGBA.cs
@@ -27,4 +27,56 @@ public struct ColorRGBA<TValue> where TValue : INumber<TValue>
     /// 해당 색상의 투명도.
     /// </summary>
     public TValue A { get; set; }
+
+    /// <summary>
+    /// 생성자.
+    /// </summary>
+    public ColorRGBA()
+    {
+        R = default(TValue);
+        G = default(TValue);
+        B = default(TValue);
+        A = default(TValue);
+    }
+
+    /// <summary>
+    /// 생성자.
+    /// </summary>
+    /// <param name="value">생성할 색상의 공통 값.</param>
+    public ColorRGBA(TValue value)
+    {
+        R = value;
+        G = value;
+        B = value;
+        A = value;
+    }
+
+    /// <summary>
+    /// 생성자.
+    /// </summary>
+    /// <param name="r">생성할 색상의 빨강 값.</param>
+    /// <param name="g">생성할 색상의 초록 값.</param>
+    /// <param name="b">생성할 색상의 파랑 값.</param>
+    public ColorRGBA(TValue r, TValue g, TValue b)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = TValue.One;
+    }
+
+    /// <summary>
+    /// 생성자.
+    /// </summary>
+    /// <param name="r">생성할 색상의 빨강 값.</param>
+    /// <param name="g">생성할 색상의 초록 값.</param>
+    /// <param name="b">생성할 색상의 파랑 값.</param>
+    /// <param name="a">생성할 색상의 투명도.</param>
+    public ColorRGBA(TValue r, TValue g, TValue b, TValue a)
+    {
+        R = r;
+        G = g;
+        B = b;
+        A = a;
+    }
 }
